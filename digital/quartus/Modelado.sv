f58e989bcd090abd0610af620eb07f07b0b2ba81
MarianoIDC <mmunoz@estudiantec.cr>
2022-01-19 21:42:41 -0600

    Update

--- digital/quartus/Modelado.sv
+++ digital/quartus/Modelado.sv
@@ -1,5 +1,20 @@
 module Modelado (
-    
+    input logic [31:0] x,
+	 output logic [31:0] y
 );
+
+logic a1 = 32'd1;
+logic b0 = 32'd2;
+logic b1 = 32'd3;
+
+logic w0 = 32'd10;
+
+int cont = 0;
+
+
+
+
+
+
     
 endmodule
